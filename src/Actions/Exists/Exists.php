1be8ea86a9fc65607ddde530dc0d310db2d2c083
ArefShojaei <arefshojaei82dev@gmail.com>
2025-04-25 23:21:45 +0430

    ADD: add new actions

---
+++ src/Actions/Exists/Exists.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Redis\Actions\Exists;
+
+use Redis\ActionInterface;
+use Redis\Storage\Storage;
+
+
+final class Exists implements ActionInterface {
+    private string $key;
+
+    
+    public function __construct(array $params) {
+        $this->key = current($params);
+    }
+
+    public function dispatch(): string {
+        $isExists = Storage::has($this->key);
+        
+        return $isExists ? "True" : "(nil)";
+    }
+}
---
+++ src/Actions/Keys/Keys.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Redis\Actions\Keys;
+
+use InvalidArgumentException;
+use Redis\ActionInterface;
+use Redis\Storage\Storage;
+
+
+final class Keys implements ActionInterface {
+    private const SYMBOL = "*";
+
+    private string $key;
+
+    
+    public function __construct(array $params) {
+        $this->key = current($params);
+
+        if ($this->key !== self::SYMBOL) throw new InvalidArgumentException("Should use the \"*\" in the command argument!");
+    }
+
+    public function dispatch(): string {
+        $values = Storage::all();
+
+        if (is_null($values)) return "(empty list or set)";
+
+        $result = "";
+
+        $count = 1;
+
+        foreach ($values as $index => $data) {
+            foreach ($data as $key => $value) {
+                $result .= "{$count}) \"{$key}\"" . PHP_EOL;
+                
+                $count++;
+            }
+        }
+
+        return $result;
+    }
+}
