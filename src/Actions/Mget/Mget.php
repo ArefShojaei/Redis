8ae6c8298e40dd0ca648017bda9641f2e8d66c65
ArefShojaei <arefshojaei82dev@gmail.com>
2025-04-30 04:07:43 +0430

    ADD: add new action for multiple data

---
+++ src/Actions/Mget/Mget.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Redis\Actions\Mget;
+
+use Redis\ActionInterface;
+use Redis\Storage\Storage;
+
+
+final class Mget implements ActionInterface {
+    private array $keys;
+
+
+    public function __construct(array $params) {
+        $this->keys = $params;
+    }
+
+    public function dispatch(): string {
+        $content = "";
+        
+        foreach ($this->keys as $index => $key) {
+            $index++;
+
+            $value = Storage::get($key);
+
+            if ($value) $content .= "{$index}) " . Storage::get($key) . PHP_EOL;
+        }
+
+        return $content ? $content : "(nil)";
+    }
+}
---
+++ src/Actions/Mset/Mset.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Redis\Actions\Mset;
+
+use Redis\ActionInterface;
+use Redis\Storage\Storage;
+
+
+final class Mset implements ActionInterface {
+    private array $inputs;
+
+
+    public function __construct(array $params) {
+        $this->inputs = array_chunk($params, 2);
+    }
+
+    public function dispatch(): string {
+        foreach ($this->inputs as $input) {
+            [$key, $value] = $input;
+
+            Storage::save($key, $value);
+        }
+
+        return true ? "True" : "(nil)";
+    }
+}
