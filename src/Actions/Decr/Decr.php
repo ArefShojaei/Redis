af384b386fcd7a1aeb5983a11b8db20b2f900bd9
ArefShojaei <arefshojaei82dev@gmail.com>
2025-04-30 04:37:28 +0430

    ADD: add new action to decrement number

---
+++ src/Actions/Decr/Decr.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Redis\Actions\Decr;
+
+use Redis\ActionInterface;
+use Redis\Storage\Storage;
+
+
+final class Decr implements ActionInterface {
+    private string $key;
+
+    public function __construct(array $params) {
+        $this->key = current($params);
+    }
+
+    public function dispatch(): string {
+        $value = Storage::get($this->key);
+
+        $newValue = $value - 1;
+
+        $isUpdated = Storage::update($this->key, $newValue);
+        
+        return $isUpdated ? $newValue : "(nil)";
+    }
+}
