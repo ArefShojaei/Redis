64d8e3fe40ebd539bf404ef6db136ba7e5a362c9
ArefShojaei <arefshojaei82dev@gmail.com>
2025-04-30 05:11:36 +0430

    ADD: add new Action to set & get data together

---
+++ src/Actions/GetSet/GetSet.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Redis\Actions\GetSet;
+
+use Redis\ActionInterface;
+use Redis\Storage\Storage;
+
+
+final class GetSet implements ActionInterface {
+    private string $key;
+
+    private string $value;
+
+
+    public function __construct(array $params) {
+        [$key, $value] = $params;
+
+        $this->key = $key;
+        
+        $this->value = $value;
+    }
+
+    public function dispatch(): string {
+        $isKeyExists = Storage::has($this->key);
+
+        if ($isKeyExists) return Storage::get($this->key);
+
+        Storage::save($this->key, $this->value);
+
+        return Storage::get($this->key);
+    }
+}
