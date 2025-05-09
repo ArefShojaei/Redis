73a62d80be5cda4d696cb1e9bf46f6e1e63d726b
ArefShojaei <arefshojaei82dev@gmail.com>
2025-05-09 23:04:29 +0430

    ADD: add new Actions

---
+++ src/Actions/DecrBy/DecrBy.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Redis\Actions\DecrBy;
+
+use Redis\ActionInterface;
+use Redis\Storage\Storage;
+
+
+final class DecrBy implements ActionInterface {
+    private string $key;
+
+    private string $value;
+
+    public function __construct(array $params) {
+        [$key, $value] = $params;
+
+        $this->key = $key;
+        
+        $this->value = $value;
+    }
+
+    public function dispatch(): string {
+        $newValue = $this->value;
+
+        $isUpdated = Storage::update($this->key, $newValue);
+        
+        return $isUpdated ? $newValue : "(nil)";
+    }
+}
---
+++ src/Actions/GetRange/GetRange.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Redis\Actions\GetRange;
+
+use Redis\ActionInterface;
+use Redis\Storage\Storage;
+
+
+final class GetRange implements ActionInterface {
+    private string $key;
+
+    private int $start;
+    
+    private int $end;
+
+    
+    public function __construct(array $params) {
+        [$key, $start, $end] = $params;
+
+        $this->key = $key;
+        
+        $this->start = $start;
+
+        $this->end = $end;
+    }
+
+    public function dispatch(): string {
+        $value = Storage::get($this->key);
+
+        return $value ? substr($value, $this->start, $this->end + 1) : "(nil)";
+    }
+}
