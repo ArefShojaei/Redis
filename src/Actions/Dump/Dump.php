52dc6cf60970939778d774b7bcd0443a19e1df63
ArefShojaei <arefshojaei82dev@gmail.com>
2025-05-10 00:21:21 +0430

    ADD: add new Action to dump data

---
+++ src/Actions/Dump/Dump.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Redis\Actions\Dump;
+
+use Redis\ActionInterface;
+use Redis\Storage\Storage;
+
+
+final class Dump implements ActionInterface {
+    public function dispatch(): string {
+        $storage = Storage::all();
+        
+        return $storage ? print_r($storage, true) : "(nil)";
+    }
+}
