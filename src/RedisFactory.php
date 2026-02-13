3ff27aeefa34ad8e9893e97f529c5d071d9215ce
ArefShojaei <arefshojaei82@gmail.com>
2026-02-13 22:52:22 +0330

    chore: implement createServer logic

--- src/RedisFactory.php
+++ src/RedisFactory.php
@@ -8,7 +8,7 @@ use Redis\Contracts\Interfaces\RedisFactory as IRedisFactory;
 final class RedisFactory implements IRedisFactory {
     private const SERVER_HOST = "127.0.0.1";
 
-    private const SERVER_PORT = 5500;
+    private const SERVER_PORT = 7000;
 
 
     private static function createInstance(array $params): Redis {
@@ -20,5 +20,12 @@ final class RedisFactory implements IRedisFactory {
 
     public static function createCli(array $params = []): Redis {
         return self::createInstance($params);
+    }
+
+    public static function createServer(string $host = self::SERVER_HOST, int $port = self::SERVER_PORT): Redis {
+        return self::createInstance([
+            "host" => $host,
+            "port" => $port
+        ]);
     }
 }
